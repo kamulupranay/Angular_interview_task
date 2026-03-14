12bb6e41922b72bb1f68e1a481d31566e6e07c5d
Kamulu Pranay <kamulupranay@gmail.com>
2026-03-14 14:05:27 +0530

    fix: resolved login form validation issues

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,8 +15,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      userName: new FormControl(null, [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
-      password: new FormControl(null, [Validators.required])
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
     });
     
   }
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
   // }
 
   onSubmit(){
-    console.log(this.loginForm.value);
+    console.log(this.loginForm);
     if(this.loginForm.value.userName == 'admin' && this.loginForm.value.password == "admin"){
       this.router.navigate(['/home'], { relativeTo: this.route });
       this.toastr.success('', 'Login Succesfully');
